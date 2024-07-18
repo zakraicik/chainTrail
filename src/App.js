6b478eabe95db7d54714dfde69183f12a8d05fb8
Zachary Raicik <zak@Zacharys-MacBook-Pro-2.local>
2024-07-18 10:20:59 -0700

    Added app loading screen

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ScrollableChain from './components/ScrollableChain'
 import BlockSummary from './components/BlockSummary'
 import TransactionSummary from './components/TransactionSummary'
 import Header from './components/Header'
+import { RiseLoader } from 'react-spinners'
 
 import { getBlockDetails, getBlockTransactions } from './helper/alchemyHelpers'
 
@@ -21,20 +22,24 @@ function App () {
   const [selectedBlock, setSelectedBlock] = useState(null)
   const [blockDetails, setBlockDetails] = useState(null)
   const [transactionDetails, setTransactionDetails] = useState(null)
+  const [loading, setLoading] = useState(true) // Add loading state
 
   useEffect(() => {
     async function getBlockNumbers () {
-      const currentBlockNumber = await alchemy.core.getBlockNumber()
-      const blockNumbersArray = []
-      for (let i = 0; i < 5; i++) {
-        blockNumbersArray.push({ id: i, blockNumber: currentBlockNumber - i })
+      try {
+        const currentBlockNumber = await alchemy.core.getBlockNumber()
+        const blockNumbersArray = []
+        for (let i = 0; i < 5; i++) {
+          blockNumbersArray.push({ id: i, blockNumber: currentBlockNumber - i })
+        }
+
+        setBlockNumbers(blockNumbersArray.reverse())
+        handleBlockSelection(blockNumbersArray.at(-1))
+      } catch (error) {
+        console.error('Error fetching block numbers', error)
+      } finally {
+        setLoading(false) // Set loading to false after data is fetched
       }
-
-      setBlockNumbers(blockNumbersArray.reverse())
-
-      console.log(blockNumbersArray)
-
-      handleBlockSelection(blockNumbersArray.at(-1))
     }
 
     getBlockNumbers()
@@ -42,22 +47,29 @@ function App () {
 
   const fetchBlockDetails = useCallback(async () => {
     if (selectedBlock) {
-      const blockDetails = await getBlockDetails(
-        selectedBlock.blockNumber,
-        alchemy
-      )
-      setBlockDetails(blockDetails)
+      try {
+        const blockDetails = await getBlockDetails(
+          selectedBlock.blockNumber,
+          alchemy
+        )
+        setBlockDetails(blockDetails)
+      } catch (error) {
+        console.error('Error fetching block details', error)
+      }
     }
   }, [selectedBlock])
 
   const fetchBlockTransactions = useCallback(async () => {
     if (blockDetails) {
-      const blockTransactions = await getBlockTransactions(
-        blockDetails,
-        alchemy
-      )
-
-      setTransactionDetails(blockTransactions)
+      try {
+        const blockTransactions = await getBlockTransactions(
+          blockDetails,
+          alchemy
+        )
+        setTransactionDetails(blockTransactions)
+      } catch (error) {
+        console.error('Error fetching block transactions', error)
+      }
     }
   }, [blockDetails])
 
@@ -77,26 +89,35 @@ function App () {
 
   return (
     <div className='App'>
-      <Header />
-      <div className='scrollable-chain-parent-container'>
-        <ScrollableChain
-          blockNumbers={blockNumbers}
-          handleBlockSelection={handleBlockSelection}
-          selectedBlock={selectedBlock}
-        />
-      </div>
-      <div className='row-container'>
-        {selectedBlock ? (
-          <>
-            <BlockSummary blockDetails={blockDetails} />
-            <TransactionSummary transactionDetails={transactionDetails} />
-          </>
-        ) : (
-          <div className='empty-information-container'>
-            <div className='no-selection'> test</div>
+      {loading ? ( // Conditionally render loading screen
+        <div className='loading-screen'>
+          <RiseLoader color='#72f491' size='30px' />
+          <p className='block-number'>App Loading</p>
+        </div>
+      ) : (
+        <>
+          <Header />
+          <div className='scrollable-chain-parent-container'>
+            <ScrollableChain
+              blockNumbers={blockNumbers}
+              handleBlockSelection={handleBlockSelection}
+              selectedBlock={selectedBlock}
+            />
           </div>
-        )}
-      </div>
+          <div className='row-container'>
+            {selectedBlock ? (
+              <>
+                <BlockSummary blockDetails={blockDetails} />
+                <TransactionSummary transactionDetails={transactionDetails} />
+              </>
+            ) : (
+              <div className='empty-information-container'>
+                <div className='no-selection'> test</div>
+              </div>
+            )}
+          </div>
+        </>
+      )}
     </div>
   )
 }
